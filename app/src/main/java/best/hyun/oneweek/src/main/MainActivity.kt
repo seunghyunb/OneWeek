517c5b13388d51b8ad77ab9c3b47afb2f0b57d4a
BAEK <izvm100@gmail.com>
2022-10-31 19:31:08 +0900

    패키지 구조 정리

---
+++ app/src/main/java/best/hyun/oneweek/src/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package best.hyun.oneweek.src.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import best.hyun.oneweek.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
