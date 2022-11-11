7b07d7a87cdcdd7dfb8d470d7ac00c77809f6f05
BAEK <izvm100@gmail.com>
2022-11-11 23:25:13 +0900

    Add Resources, Toolbar 리소스 추가, 툴바 추가

--- app/src/main/java/best/hyun/oneweek/src/splash/SplashActivity.kt
+++ app/src/main/java/best/hyun/oneweek/src/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package best.hyun.oneweek.src.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import best.hyun.oneweek.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
